4e5672b6e24683d11875a57853db85e7db0ed2ae
dominic daryll <daryllbastienne32@gmail.com>
2024-11-01 10:04:56 +1300

    updated sql entries to cities

--- SQLite.sql
+++ SQLite.sql
@@ -1,2 +1,11 @@
 -- SQLite
-select * from TemperatureReadings
+-- select * from TemperatureReadings
+
+-- delete from sensors where id BETWEEN 3 and 12
+-- go
+update sensors 
+set sensor_name = "Palmerston North"
+where id ="2"
+go
+select * from sensors
+go
